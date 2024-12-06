b17741a27e85e9b6bd38ab0cc8d4dabf3bec03b1
tega <alessiotegoni4@gmail.com>
2024-12-06 15:50:30 +0100

    add: react router dom routes, pages components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Contact, Home, Layout, Posts } from "./components/_root/pages";
 
+const App = () => (
+  <Routes>
+    <Route path="/">
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<Contact />} />
+        <Route path="posts" element={<Posts />} />
+      </Route>
+    </Route>
+  </Routes>
+);
 
-function App() {
-
-
-  return <></>
-}
-
-export default App
+export default App;
---
+++ src/components/_root/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Layout } from "./Layout";
+export { default as Home } from "./Home";
+export { default as Contact } from "./Contact";
+export { default as Posts } from "./Posts.jsx";
