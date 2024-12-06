fda9fddf8af379af0f923c60d8f342e96b300283
tega <alessiotegoni4@gmail.com>
2024-12-06 16:31:42 +0100

    add: posts to posts page

--- src/components/_root/layouts/Layout.jsx
+++ src/components/_root/layouts/Layout.jsx
@@ -1,43 +1,52 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import { navLinks } from "../../constants/index";
 
 const Layout = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Navbar
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span classNameName="navbar-toggler-icon">
-            <img
-              src="https://images.vexels.com/media/users/3/155474/isolated/preview/4e12cd94f7591c3c851fce62fdc3d463-x-cross-doodle-icon.png"
-              alt="x icon"
-              width={20}
-            />
-          </span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            {navLinks.map((navLink) => (
-              <li className="nav-item">
-                <NavLink to={navLink.value} className="nav-link">
-                  {navLink.label}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+    <>
+      <header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              React router
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span classNameName="navbar-toggler-icon">
+                <img
+                  src="https://images.vexels.com/media/users/3/155474/isolated/preview/4e12cd94f7591c3c851fce62fdc3d463-x-cross-doodle-icon.png"
+                  alt="x icon"
+                  width={20}
+                />
+              </span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                {navLinks.map((navLink) => (
+                  <li className="nav-item">
+                    <NavLink to={navLink.value} className="nav-link">
+                      {navLink.label}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className="my-5">
+        <div className="container">
+          <Outlet />
         </div>
-      </div>
-    </nav>
+      </main>
+    </>
   );
 };
 export default Layout;
